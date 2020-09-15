51dfcde61516355fa7e57250f24ae30063d2d889
RafaelaCurti <70167337+RafaelaCurti@users.noreply.github.com>
2020-09-15 02:15:14 -0300

    Exemplos 1 e 2

---
+++ Exemplo1/Exemplo1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static Util.Calc;
+using static System.Console;
+namespace Exemplo1._1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = Somar(3, 5);
+            WriteLine(r);
+            ReadLine();
+        }
+    }
+}
---
+++ Exemplo2/Exemplo2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace Exemplo2
+{
+    public class Pessoa
+    {
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+    }
+    public class Programa
+    {
+        static void Main(string[] args)
+        {
+            var p = new Pessoa()
+            {
+                Matricula = 123,
+                Nome = "Guinther",
+                SobreNome = "Pauli",
+            };
+            WriteLine(string.Format("Bem-vindo {0} {1} ({2}), este é o C# 6", 
+                p.Nome, p.SobreNome, p.Matricula));
+                ReadLine();
+        }
+    }
+}
