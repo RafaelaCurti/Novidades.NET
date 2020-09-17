593ce3a4db30beb72ccb39dbdcf680a474232da1
RafaelaCurti <70167337+RafaelaCurti@users.noreply.github.com>
2020-09-17 02:29:15 -0300

    Exemplo 3

---
+++ Exemplo3/Exemplo3/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplo3
+//Jeito 1
+//{
+//class Aluno
+//{
+//    private int _Matricula;
+//    private string _Nome;
+
+//    public int Matricula
+//    {
+//    get { return this._Matricula; }
+//    set { this._Matricula = value; }
+//    }
+//    public string Nome
+//    {
+//       get { return this._Nome; }
+//       set { this._Nome = value; }
+//}
+//public Aluno (int matricula, string nome)
+//{
+//this._Nome = nome;
+// this.Matricula = matricula; 
+//}
+//}
+//}
+//Jeito2
+{
+    public class Aluno
+    {
+        public static int Matricula { get; set; } = 0;
+        public string Nome { get; set; } = "sem nome";
+        //SE
+        public string Status { get; set; } = Matricula == 0? "Não Matriculado" : "Matriculado";
+    }
+}
---
+++ Exemplo3/Exemplo3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace Exemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gp = new Aluno(); //{ Nome = "Rafaela", Matricula = 11921264 };
+            WriteLine($"{Aluno.Matricula} - {gp.Nome} - {gp.Status}");
+            ReadLine();
+        }
+    }
+}
